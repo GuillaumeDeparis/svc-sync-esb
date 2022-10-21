f5ae7a58b97a7b90042841e7e495b0b2c626123a
guilaumedeparis <deparis.guillaume@gmail.com>
2022-10-21 16:50:40 +0200

    Initialisation du projet

--- src/main/java/com/emothep/esb/IntegrationServer.java
+++ src/main/java/com/emothep/esb/IntegrationServer.java
@@ -1,11 +1,20 @@
 package com.emothep.esb;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class IntegrationServer {
+
+    private static final Logger logger = LogManager.getLogger(IntegrationServer.class);
     private String name;
-    private Path sagHome;
+    private String sagHome;
     private Path packagePath;
 
     private ArrayList<ISPackage> packageList;
@@ -21,8 +30,7 @@ public class IntegrationServer {
     }
 
     private void getPackagePath(){
-
-        this.packagePath = Path(this.sagHome + System.lineSeparator() + "packages");
+        this.packagePath = Paths.get(this.sagHome + System.lineSeparator() + "packages");
     }
 
     public String getName() {
@@ -33,11 +41,11 @@ public class IntegrationServer {
         this.name = name;
     }
 
-    public Path getSagHome() {
+    public String getSagHome() {
         return sagHome;
     }
 
-    public void setSagHome(Path sagHome) {
+    public void setSagHome(String sagHome) {
         this.sagHome = sagHome;
     }
 
@@ -45,11 +53,12 @@ public class IntegrationServer {
         return this.packageList;
     }
 
-    public void setPackageList() {
-        for (listDir in this.packagePath():
-             ) {
-            ISPackage currentPackage = new ISPackage(listDir.getName());
-            this.packageList.add(currentPackage);
+    public void setPackageList() throws IOException {
+        try(Stream<Path> listPackages = Files.walk(this.packagePath,1)){
+            listPackages
+                    .filter(Files::isDirectory)
+                    .forEach(currentPackage ->
+                            this.packageList.add(new ISPackage(currentPackage.getFileName().toString())));
         }
     }
 }
---
+++ src/test/java/com/emothep/esb/IntegrationServerTest.java
@@ -0,0 +1,11 @@
+package com.emothep.esb;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntegrationServerTest {
+    @Test
+    void name() {
+    }
+}
